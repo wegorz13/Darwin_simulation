5c17d70ffe529d9957649175640fe06b45b28590
Kacper Kopiec <kkopiec@student.agh.edu.pl>
2024-10-28 16:05:24 +0100

    lab2 + poprawki lab1

---
+++ oolab/src/main/java/agh/ics/oop/model/Vector2d.java
@@ -0,0 +1,66 @@
+package agh.ics.oop.model;
+
+import java.util.Objects;
+
+public class Vector2d {
+    private final int x, y;
+
+    public Vector2d(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    @Override
+    public String toString() {
+        return "(" + x + "," + y + ")";
+    }
+
+    public boolean precedes(Vector2d other) {
+        return x <= other.x && y <= other.y;
+    }
+
+    public boolean follows(Vector2d other) {
+        return other.x <= x && other.y <= y;
+    }
+
+    public Vector2d add(Vector2d other) {
+        return new Vector2d(x + other.x, y + other.y);
+    }
+
+    public Vector2d subtract(Vector2d other) {
+        return new Vector2d(x - other.x, y - other.y);
+    }
+
+    public Vector2d upperRight(Vector2d other) {
+        return new Vector2d(Math.max(x, other.x), Math.max(y, other.y));
+    }
+
+    public Vector2d lowerLeft(Vector2d other) {
+        return new Vector2d(Math.min(x, other.x), Math.min(y, other.y));
+    }
+
+    public Vector2d opposite() {
+        return new Vector2d(-x, -y);
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (this == other) return true;
+        if (other == null || getClass() != other.getClass()) return false;
+        Vector2d vector2d = (Vector2d) other;
+        return x == vector2d.x && y == vector2d.y;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
+    }
+}
